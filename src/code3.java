1b7a53bad01c5a8e70742fce35f84279b171b112
tcsvaishnavi <vaishnavideotale86@gmail.com>
2026-08-04 19:37:23 +0530

    want to just check

--- src/code3.java
+++ src/code3.java
@@ -3,6 +3,7 @@ public class code3 {
     public static void main(String[] args) {
 
         System.out.println("Message feature added");
+        System.out.println("i want to practice merge conflits that why i'm using this");
 
     }
 }
